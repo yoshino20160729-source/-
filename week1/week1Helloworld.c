68e638a98b607e8b570566a5b234aae5aa845656
yoshino20160729-source <yoshino20160729@gmail.com>
2025-10-12 10:51:21 +0800

    Add files via upload

---
+++ week1/week1Helloworld.c
@@ -0,0 +1,22 @@
+//#include<stdio.h>
+//int main()
+//{
+//	printf("当输入-1时，程序退出；输入0时，输出“helloworld”；当输入1时，输出“HELLOWORLD”\n");
+//	while (1)
+//	{
+//		int i;
+//		scanf_s("%d", &i);
+//		if (i == -1)
+//			break;
+//		if (i == 0)
+//		{
+//			printf("helloworld\n");}
+//		if (i == 1)
+//		{
+//			printf("HELLOWORLD\n");
+//		}
+//
+//
+//}
+//return 0;
+//}
